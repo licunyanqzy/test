d807e63f912aebc59d570ce75c7224b6c25cf868
licunyanqzy <licunyanqzy@outlook.com>
2020-08-11 22:53:47 +0800

    8.11 test

--- models.py
+++ models.py
@@ -28,31 +28,36 @@ def make_mlp(dim_list, activation="relu", batch_norm=True, dropout=0):
 
 class ActionEncoder(nn.Module):
     def __init__(
-            self, obs_len, action_lstm_input_dim, action_lstm_hidden_dim,
+            self, obs_len, action_encoder_input_dim, action_encoder_hidden_dim, embedding_dim,
     ):
         super(ActionEncoder, self).__init__()
         self.obs_len = obs_len
-        self.action_lstm_input_dim = action_lstm_input_dim
-        self.action_lstm_hidden_dim = action_lstm_hidden_dim
+        self.action_encoder_input_dim = action_encoder_input_dim
+        self.action_encoder_hidden_dim = action_encoder_hidden_dim
+        self.embedding_dim = embedding_dim
 
-        self.actionLSTM = nn.LSTMCell(self.action_lstm_input_dim, self.action_lstm_hidden_dim)
+        self.actionEncoderLSTM = nn.LSTMCell(self.action_encoder_input_dim, self.action_encoder_hidden_dim)
+        self.inputEmbedding = nn.Linear(2, self.embedding_dim)
 
     def init_hidden(self, batch):
         return (        # torch.zeros or torch.randn ?
-            torch.randn(batch, self.action_lstm_hidden_dim).cuda(),
-            torch.randn(batch, self.action_lstm_hidden_dim).cuda(),
+            torch.randn(batch, self.action_encoder_hidden_dim).cuda(),
+            torch.randn(batch, self.action_encoder_hidden_dim).cuda(),
         )
 
-    def forward(self, obs_traj):
+    def forward(self, obs_traj):    # 先embedding,后LSTM,可能存在维度问题 ?
         batch = obs_traj.shape[1]
         action_lstm_hidden_state, action_lstm_cell_state = self.init_hidden(batch)
 
+        obs_traj_embedding = self.inputEmbedding(obs_traj.contiguous().view(-1,2))
+        obs_traj_embedding = obs_traj_embedding.view(-1, batch, self.embedding_dim)
+
         for i, input_data in enumerate(
-            obs_traj[: self.obs_len].chunk(
-                obs_traj[: self.obs_len].size(0), dim=0
+            obs_traj_embedding[: self.obs_len].chunk(
+                obs_traj_embedding[: self.obs_len].size(0), dim=0
             )
         ):
-            action_lstm_hidden_state, action_lstm_cell_state = self.actionLSTM(
+            action_lstm_hidden_state, action_lstm_cell_state = self.actionEncoderLSTM(
                 input_data.squeeze(0), (action_lstm_hidden_state, action_lstm_cell_state)
             )
 
@@ -61,35 +66,55 @@ class ActionEncoder(nn.Module):
 
 class GoalEncoder(nn.Module):
     def __init__(
-            self, obs_len, goal_lstm_input_dim, goal_lstm_hidden_dim,
+            self, obs_len, goal_encoder_input_dim, goal_encoder_hidden_dim, embedding_dim
     ):
         super(GoalEncoder, self).__init__()
         self.obs_len = obs_len
-        self.goal_lstm_input_dim = goal_lstm_input_dim
-        self.goal_lstm_hidden_dim = goal_lstm_hidden_dim
+        self.goal_encoder_input_dim = goal_encoder_input_dim
+        self.goal_encoder_hidden_dim = goal_encoder_hidden_dim
+        self.embedding_dim = embedding_dim
 
-        self. goalLSTM = nn.LSTMCell(self.goal_lstm_input_dim, self.goal_lstm_hidden_dim)
+        self.goalEncoderLSTM = nn.LSTMCell(self.goal_encoder_input_dim, self.goal_encoder_hidden_dim)
+        self.inputEmbedding = nn.Linear(2, self.embedding_dim)
 
     def init_hidden(self, batch):
         return (
-            torch.randn(batch, self.goal_lstm_hidden_dim).cuda(),
-            torch.randn(batch, self.goal_lstm_hidden_dim).cuda()
+            torch.randn(batch, self.goal_encoder_hidden_dim).cuda(),
+            torch.randn(batch, self.goal_encoder_hidden_dim).cuda()
         )
 
-    def forward(self, obs_goal):
+    def forward(self, obs_goal):    # 先embedding,后LSTM,可能存在维度问题 ?
         batch = obs_goal.shape[1]
         goal_lstm_hidden_state, goal_lstm_cell_state = self.init_hidden(batch)
 
+        obs_goal_embedding = self.inputEmbedding(obs_goal.contiguous().view(-1, 2))
+        obs_goal_embedding = obs_goal_embedding.view(-1, batch, self.embedding_dim)
+
         for i, input_data in enumerate(
-            obs_goal[: self.obs_len].chunk(
-                obs_goal[: self.obs_len].size(0), dim=0
+            obs_goal_embedding[: self.obs_len].chunk(
+                obs_goal_embedding[: self.obs_len].size(0), dim=0
             )
         ):
-            goal_lstm_hidden_state, goal_lstm_cell_state = self.goalLSTM(
-                obs_goal, (goal_lstm_hidden_state, goal_lstm_cell_state)
+            goal_lstm_hidden_state, goal_lstm_cell_state = self.goalEncoderLSTM(
+                input_data.squeeze(0), (goal_lstm_hidden_state, goal_lstm_cell_state)
             )
 
         return goal_lstm_hidden_state
+
+
+class GoalDecoder(nn.Module):
+    def __init__(
+            self, pred_len, goal_decoder_input_dim, goal_decoder_hidden_dim,
+    ):
+        super(GoalDecoder, self).__init__()
+        self.pred_len = pred_len
+        self.goal_decoder_input_dim = goal_decoder_input_dim
+        self.goal_decoder_hidden_dim = goal_decoder_hidden_dim
+
+        self.goalDecoderLSTM = nn.LSTMCell(self.goal_decoder_input_dim, self.goal_decoder_hidden_dim)
+        self.hidden2pos = nn.Linear(self.goal_decoder_hidden_dim, 2)
+
+    def forward(self, ):
 
 
 
@@ -100,36 +125,48 @@ class Attention(nn.Module):
 
 
 class ActionDecoder(nn.Module):
-
-
-
-
-class GoalDecoder(nn.Module):
     def __init__(self, ):
-        super(GoalDecoder, self).__init__()
+        super(ActionDecoder, self).__init__()
 
 
 
 class TrajectoryPrediction(nn.Module):
     def __init__(
             self, obs_len, pred_len,
-            action_lstm_input_dim, action_lstm_hidden_dim, goal_lstm_input_dim, goal_lstm_hidden_dim,
+            action_encoder_input_dim, action_encoder_hidden_dim, action_embedding_dim,
+            goal_encoder_input_dim, goal_encoder_hidden_dim, goal_embedding_dim,
             dropout, noise_dim=(8,), noise_type="gaussian",
     ):
         super(TrajectoryPrediction, self).__init__()
 
         self.obs_len = obs_len
         self.pred_len = pred_len
+        self.action_encoder_input_dim = action_encoder_input_dim
+        self.action_encoder_hidden_dim = action_encoder_hidden_dim
+        self.action_embedding = action_embedding_dim
+        self.goal_encoder_input_dim = goal_encoder_input_dim
+        self.goal_encoder_hidden_dim = goal_encoder_hidden_dim
+        self.goal_embedding_dim = goal_embedding_dim
 
-        self.actionEncoder = ActionEncoder()
-        self.goalEncoder = GoalEncoder()
-        self.attention = Attention()
+        self.actionEncoder = ActionEncoder(
+            obs_len=self.obs_len,
+            action_encoder_input_dim=self.action_encoder_input_dim,
+            action_encoder_hidden_dim=self.action_encoder_hidden_dim,
+            embedding_dim=self.action_embedding
+        )
+        self.goalEncoder = GoalEncoder(
+            obs_len=self.obs_len,
+            goal_encoder_input_dim=self.goal_encoder_input_dim,
+            goal_encoder_hidden_dim=self.goal_encoder_hidden_dim,
+            embedding_dim=self.goal_embedding_dim
+        )
 
 
 
 
 
-    def forward(self, ):
+
+    def forward(self, training_step, ):
 
 
 
